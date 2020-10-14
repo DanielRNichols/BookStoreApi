c579ce06644be4ed0a344fb3120714b9f5221ec9
DanielRNichols <danielrnichols63@gmail.com>
2020-10-14 17:25:11 -0500

    Initial setup for BookStore-UI project

---
+++ BookStore-UI/Static/EndPoints.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_UI.Static
+{
+    public static class EndPoints
+    {
+        public static string BaseUrl = "https://localhost:44349/";
+        public static string ApiBaseUrl = $"{BaseUrl}api/";
+        public static string AuthorsEndPoint = $"{ApiBaseUrl}authors/";
+        public static string BooksEndPoint = $"{ApiBaseUrl}books/";
+        public static string UsersEndPoint = $"{ApiBaseUrl}users/";
+        public static string LoginEndPoint = $"{UsersEndPoint}login/";
+        public static string RegisterEndPoint = $"{UsersEndPoint}register/";
+
+    }
+}
